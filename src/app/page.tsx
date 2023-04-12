d6c0243b25014da22ce903ea574e22db4f73edbd
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-04-12 15:35:13 +0500

    break all in step 2 for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,52 +2,57 @@ import RainbowKitBTN from "./rainbowkit/page";
 import EthersButton from "./ethers/page";
 import Link from "next/link";
 
-export default function Home() {
+export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col min-h-screen  justify-start items-center w-full  ">
-        <div className="flex flex-col justify-start items-center w-full py-8 space-y-6 text-center">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-24">
+      <div className="flex flex-col min-h-screen justify-start items-center w-full">
+        <div className="flex flex-col min-h-screen justify-start items-center w-full py-8 md:py-12 space-y-6 md:space-y-10 text-center">
           <div>
-            {" "}
-            <h1> Step XXX Account Abstraction </h1>
+            <h1 className="text-4xl md:text-6xl font-bold leading-tight">
+              Account Abstraction Task
+            </h1>
           </div>
-          <div className="pt-3">
-            {" "}
-            <h4>
-              {" "}
-              basic_account_creation_trans + mint_nft + create_wallet_social{" "}
+          <div className="mt-6">
+            <h4 className="text-lg md:text-2xl font-semibold">
+              Concepts. Transactions. Mint NFTs. Social-Wallets
             </h4>
           </div>
-          <div className="flex flex=col md: flex-row justify-center items-center gap-5">
-            {" "}
-            {/* <div>
-              <WagmiButton />
-            </div> */}
+
+          <div className="flex flex-col md:flex-row justify-center items-center gap-5">
             <div>
               <RainbowKitBTN />
+              <span className="text-sm">Rainbow Kit</span>
+              {/* </RainbowKitBTN> */}
             </div>
             <div>
               <EthersButton />
+              <span className="text-sm">Ethers API</span>
+              {/* </EthersButton> */}
             </div>
           </div>
-          <div className="flex flex-col flex-wrap gap-6 bg-gray-50 items-stretch w-full border-gray-200/95 rounded-sm  border-4 border-t-8 py-8 mt-8">
-            <div>
-              <h3>Step 2: Mint NFT</h3>
-              <div className="flex gap-6 flex-col px-4 items-start justify-start">
-                <p>
-                  <Link href="https://mumbai.polygonscan.com/address/0x6fbD5Ed295495750F9B3433d45092535FbD19EcF#tokentxnsErc721">
-                    Gasless AA Trans see under ERC-721 Token Txns
-                  </Link>
-                </p>
-                <p>
-                  Smart Contract Address:
-                  0x6fbD5Ed295495750F9B3433d45092535FbD19EcF
-                </p>
-              </div>
+          <div className="flex flex-wrap justify-center items-stretch w-full p-8 mt-8 bg-gray-50 rounded-md">
+            <div className="flex flex-col p-6 m-2 bg-white border border-gray-200/95 rounded-md relative w-full md:w-4/5">
+              <span className="text-6xl font-bold text-gray-200 absolute top-0 left-0 opacity-10">
+                2
+              </span>
+              <h3 className="pb-4 break-all">Step 2: Mint NFT</h3>
+              <p>
+                <Link href="https://mumbai.polygonscan.com/address/0x6fbD5Ed295495750F9B3433d45092535FbD19EcF#tokentxnsErc721">
+                  Gasless AA Trans see under ERC-721 Token Txns
+                </Link>
+              </p>
+              <p className="break-all">
+                <b>Smart Contract Address:</b>
+                0x6fbD5Ed295495750F9B3433d45092535FbD19EcF
+              </p>
             </div>
-            <div>
-              <h3>Step 1 basic_account_creation_trans</h3>
-              <div className="flex gap-6 flex-col px-4 items-start justify-start">
+            <div className="flex flex-col p-6 m-2 bg-white border border-gray-200/95 rounded-md relative w-full md:w-4/5">
+              <span className="text-6xl font-bold text-gray-200 absolute top-0 left-0 opacity-10">
+                1
+              </span>
+              <h3 className="pb-4">Step 1 account_creation_trans</h3>
+
+              <div className=" break-all flex gap-6 flex-col px-4 items-start justify-start">
                 <div>
                   <Link href="https://goerli.etherscan.io/tx/0x33e1afb40b7cd52029f79350fc4c10b32d900a9bdd2b89ac3a2d6383fb90164c">
                     <p>Funded Goreli ETH In Meta Mask Wallet</p>
@@ -76,7 +81,7 @@ export default function Home() {
                   </Link>
                 </div>
                 <div>
-                  <Link href="https://goerli.etherscan.io/tx/0xc75d9e0a6ecc7294c6f6fca9896530235e48c1e4a40a37f6ae7396317f552e08">
+                  <Link href="https://goerli.etherscan.io/tx/0xcca6ddd1f4b17930fca1447b6795e8ffe5f29065da81c5670c4dfd6a954e8049">
                     <p>
                       <b>Batch Transfer ETH:</b>
                       0xcca6ddd1f4b17930fca1447b6795e8ffe5f29065da81c5670c4dfd6a954e8049
